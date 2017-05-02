4fb6ea959f7092b5f6c56c8d1c4bb4dec59964ac
dybiszb <bartlomiej.dybisz@gmail.com>
2017-05-02 20:43:49 +0200

    Project structure setup.

---
+++ code/conway_app/main.cpp
@@ -0,0 +1,7 @@
+#include "conway.h"
+
+
+int main() {
+    cgl::print_test_string();
+    return 0;
+}
---
+++ code/conway_lib/conway.h
@@ -0,0 +1,15 @@
+#ifndef CONWAY_H
+#define CONWAY_H
+
+#include <iostream>
+
+#ifdef _OPENMP
+    #include <omp.h>
+#endif
+
+namespace cgl {
+    void print_test_string();
+}
+
+
+#endif
